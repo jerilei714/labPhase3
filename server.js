0926fcdecac950832184f5c6917ac8f43e053172
Restyled.io <commits@restyled.io>
2024-03-29 05:11:09 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const hbsCtrl = require('hbs')
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const cookieParser = require('cookie-parser')
 
 const routes = require('./Routes/routes')
-const { connectToDB } = require('./Model/labDatabase');
+const {connectToDB} = require('./Model/labDatabase');
 const registrationController = require('./Controller/regController');
 const loginController = require('./Controller/loginController');
 const userController = require('./Controller/userController');
@@ -17,30 +17,28 @@ const reservationController = require('./Controller/reservationController');
 const seatController = require('./Controller/seatController');
 const redSeatsController = require('./Controller/redSeatsController');
 const reserveSlotsController = require('./Controller/reserveSlotsController');
-const { router, cookieAuth } = require('./Middleware/cookieAuth'); 
-const Authenticated = require('./Middleware/authenticated'); 
-const { initializeApplication } = require('./Middleware/initialize'); 
+const {router, cookieAuth} = require('./Middleware/cookieAuth');
+const Authenticated = require('./Middleware/authenticated');
+const {initializeApplication} = require('./Middleware/initialize');
 const app = express();
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({limit : '5mb'}));
+app.use(express.urlencoded({limit : '5mb', extended : true}));
 app.use('/', express.static(path.join(__dirname, 'Public')));
 app.use(cookieParser());
-app.use(cookieAuth); 
+app.use(cookieAuth);
 app.use(Authenticated);
 const hbs = exphbs.create({
-    extname: '.hbs',
-    defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'View', 'layouts'),
-    partialsDir: path.join(__dirname, 'View', 'partials')
-  });
-  
+  extname : '.hbs',
+  defaultLayout : 'main',
+  layoutsDir : path.join(__dirname, 'View', 'layouts'),
+  partialsDir : path.join(__dirname, 'View', 'partials')
+});
+
 hbsCtrl.registerPartials(path.join(__dirname, 'View', 'partials'))
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'View'));
-app.use((req,res,next) =>{
-    next()
-})
+app.use((req, res, next) => {next()})
 
 app.use('/', routes)
 
@@ -48,15 +46,15 @@ app.use('/register', registrationController);
 app.use('/login', loginController);
 app.use('/users', userController);
 app.use('/labs', labController);
-app.use('/reservations', reservationController); 
+app.use('/reservations', reservationController);
 app.use('/seats', seatController);
 app.use('/reservedseats', redSeatsController);
 app.use('/reserve-slots', reserveSlotsController);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    // UNTICK IF YOU WANT TO RESET DATABASE CONTENTS
-    //comment out the line below to prevent database reset
-    initializeApplication() 
+  console.log(`Server running on port ${PORT}`);
+  // UNTICK IF YOU WANT TO RESET DATABASE CONTENTS
+  // comment out the line below to prevent database reset
+  initializeApplication()
 });
