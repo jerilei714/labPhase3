2476b16c41f30d85715e4bf3546f71348e0ef116
Restyled.io <commits@restyled.io>
2024-03-31 07:44:32 +0000

    Restyled by prettier

--- Controller/reservationController.js
+++ Controller/reservationController.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const {ObjectId} = require('mongodb');
+const express = require("express");
+const { ObjectId } = require("mongodb");
 const {
   createReservation,
   getReservation,
   updateReservation,
   deleteReservation,
   getReservedSeatsByUsername,
-  getReservationsByUsername
-} = require('../Model/labReserves');
-const {updateSeatStatus} = require('../Model/labSeats');
-const {createReservedSeat, checkSeatAvailability} =
-    require('../Model/labReservedSeats');
+  getReservationsByUsername,
+} = require("../Model/labReserves");
+const { updateSeatStatus } = require("../Model/labSeats");
+const {
+  createReservedSeat,
+  checkSeatAvailability,
+} = require("../Model/labReservedSeats");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const {
       lab_id,
@@ -24,131 +26,138 @@ router.post('/', async (req, res) => {
       reserve_time,
       seat_number,
       tnd_requested,
-      anonymous
+      anonymous,
     } = req.body;
     const reservationData = {
       lab_id,
       lab_name,
       user_id,
-      username : anonymous ? 'Anonymous' : username,
+      username: anonymous ? "Anonymous" : username,
       reserve_date,
       reserve_time,
       seat_number,
-      anonymous
+      anonymous,
     };
     const reservationId = await createReservation(reservationData);
     const reservedSeatData = {
       lab_id,
-      reservation_id : reservationId,
+      reservation_id: reservationId,
       seat_number,
-      username : username,
+      username: username,
       reserve_date,
       reserve_time,
       tnd_requested,
-      anonymous
+      anonymous,
     };
     await createReservedSeat(reservedSeatData);
-    await updateSeatStatus(lab_name, seat_number, 'Reserved');
-    res.status(201).json({reservationId});
+    await updateSeatStatus(lab_name, seat_number, "Reserved");
+    res.status(201).json({ reservationId });
   } catch (error) {
-    console.error('Error creating reservation:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error creating reservation:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.get('/checkAvailability', async (req, res) => {
-  const {date, labId, seatNumber} = req.query;
+router.get("/checkAvailability", async (req, res) => {
+  const { date, labId, seatNumber } = req.query;
   const isAvailable = await checkSeatAvailability(labId, seatNumber, date);
-  res.json({isAvailable});
+  res.json({ isAvailable });
 });
 
-router.get('/:reservationId', async (req, res) => {
+router.get("/:reservationId", async (req, res) => {
   try {
-    const {reservationId} = req.params;
+    const { reservationId } = req.params;
     const reservation = await getReservation(new ObjectId(reservationId));
     if (reservation) {
       res.json(reservation);
     } else {
-      res.status(404).json({error : 'Reservation not found'});
+      res.status(404).json({ error: "Reservation not found" });
     }
   } catch (error) {
-    console.error('Error getting reservation:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error getting reservation:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.put('/:reservationId', async (req, res) => {
+router.put("/:reservationId", async (req, res) => {
   try {
-    const {reservationId} = req.params;
+    const { reservationId } = req.params;
     const updatedReservationData = req.body;
-    const success =
-        await updateReservation(reservationId, updatedReservationData);
+    const success = await updateReservation(
+      reservationId,
+      updatedReservationData,
+    );
     if (success) {
-      res.json({success : true});
+      res.json({ success: true });
     } else {
-      res.status(404).json({error : 'Reservation not found'});
+      res.status(404).json({ error: "Reservation not found" });
     }
   } catch (error) {
-    console.error('Error updating reservation:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error updating reservation:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.get('/userReservations/:username', async (req, res) => {
+router.get("/userReservations/:username", async (req, res) => {
   try {
-    const {username} = req.params;
+    const { username } = req.params;
     const userReservations = await getReservedSeatsByUsername(username);
-    res.json({userReservations});
+    res.json({ userReservations });
   } catch (error) {
-    console.error('Error fetching user reservations:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error fetching user reservations:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.put('/update/:reservationId', async (req, res) => {
+router.put("/update/:reservationId", async (req, res) => {
   try {
-    const {reservationId} = req.params;
+    const { reservationId } = req.params;
     const updatedDetails = req.body;
-    const success =
-        await updateReservation(new ObjectId(reservationId), updatedDetails);
+    const success = await updateReservation(
+      new ObjectId(reservationId),
+      updatedDetails,
+    );
     if (success) {
-      res.json({success : true, message : 'Reservation updated successfully'});
+      res.json({ success: true, message: "Reservation updated successfully" });
     } else {
-      res.status(404).json({error : 'Failed to update reservation'});
+      res.status(404).json({ error: "Failed to update reservation" });
     }
   } catch (error) {
-    console.error('Error updating reservation:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error updating reservation:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.get('/byUsername/:username', async (req, res) => {
+router.get("/byUsername/:username", async (req, res) => {
   try {
-    const {username} = req.params;
-    const {labId, reserveDate} = req.query;
-    const userReservations =
-        await getReservationsByUsername(username, labId, reserveDate);
-    res.json({userReservations});
+    const { username } = req.params;
+    const { labId, reserveDate } = req.query;
+    const userReservations = await getReservationsByUsername(
+      username,
+      labId,
+      reserveDate,
+    );
+    res.json({ userReservations });
   } catch (error) {
-    console.error('Error fetching user reservations:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error fetching user reservations:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 // commented in case
 
-router.delete('/:reservationId', async (req, res) => {
+router.delete("/:reservationId", async (req, res) => {
   try {
-    const {reservationId} = req.params;
+    const { reservationId } = req.params;
     const success = await deleteReservation(reservationId);
     if (success) {
-      res.json({success : true});
+      res.json({ success: true });
     } else {
-      res.status(404).json({error : 'Reservation not found'});
+      res.status(404).json({ error: "Reservation not found" });
     }
   } catch (error) {
-    console.error('Error deleting reservation:', error);
-    res.status(500).json({error : 'Internal server error'});
+    console.error("Error deleting reservation:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
